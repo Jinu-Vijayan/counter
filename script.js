c6f3787af3e76057e1d1235ab696c50ef18f5511
Jinu Vijayan <jinuvijayan01@gmail.com>
2024-01-11 09:29:57 +0530

    Add:included keyboard shortcuts and help button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let showVal = document.querySelector("#show-val");
 let btnContainer = document.querySelector("#btn-container");
+let helpBtn = document.querySelector("#help-btn")
+let helpInfo = document.querySelector("#help-info")
 let count = 0;
 btnContainer.addEventListener("click",(e) => {
     if(e.target.id == "increase"){
@@ -15,4 +17,24 @@ btnContainer.addEventListener("click",(e) => {
         showVal.style.color = "black";
     }
     showVal.innerText = count;
+})
+
+document.addEventListener("keydown", (e) => {
+    if(e.key == "+"){
+        count++;
+        showVal.style.color = "green";
+    } else if ( e.key == "r" || e.key == "R"){
+        count = 0;
+    } else if (e.key == "-"){
+        count--;
+        showVal.style.color = "red";
+    }
+    if(count == 0){
+        showVal.style.color = "black";
+    }
+    showVal.innerText = count;
+})
+
+helpBtn.addEventListener('click',(e)=>{
+    helpInfo.classList.toggle("hidden");
 })
